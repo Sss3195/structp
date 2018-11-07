9ce80054dc864705d5a7ef91ea5e3e6cffcadf65
Sss3195 <s20171103195@danqianfang.local>
2018-11-07 09:13:02 +0800

    Initial Commit

---
+++ structp/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  structp
+//
+//  Created by s20171103195 on 2018/11/7.
+//  Copyright © 2018 s20171103195. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
